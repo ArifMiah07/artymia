5edc8e293fc26be3abe2a49cc71275b23adb184f
ArifMiah07 <arifmiah.me322@gmail.com>
2024-08-19 22:44:18 +0600

    add swiper

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -3,81 +3,84 @@ import { Carousel } from 'react-responsive-carousel';
 
 const Banner = () => {
     return (
-        <div className="relative">
-            <div className="absolute inset-0 flex items-center justify-center z-50">
-                <p className="text-3xl">Explore</p>
+        <div className="relative group">
+            {/* Text that will be visible on hover */}
+            <div className="absolute inset-0 flex flex-col p-3 justify-center items-center z-50 transition-opacity duration-300 opacity-0 group-hover:opacity-100">
+                <h1 className="p-5 bg-magenta-breeze text-3xl font-bungeeTint">Welcome to Artymia</h1>
+                <p className="p-5 bg-forest-sunrise mb-8 font-montserrat">Explore our curated collection of standout AI-generated artworks. Discover unique pieces, handpicked for their creativity and impact.</p>
             </div>
-            <div>
-            <Carousel
-            autoPlay={true}
-            interval={2000}
-            infiniteLoop={true}
-            >
-                <div>
-                    <img src="https://ideogram.ai/assets/image/lossless/response/-sh1EEQ1RyyDohJH_qj42w" />
-                    <p className="legend">Legend 1</p>
-                </div>
-                <div>
-                    <img src="https://ideogram.ai/assets/progressive-image/balanced/response/ZQ5XnjQWQPWY-WKQ3t7xKA" />
-                    <p className="legend">Legend 2</p>
-                </div>
-                <div>
-                    <img src="https://ideogram.ai/assets/progressive-image/balanced/response/aK6OIb67QmCHZYZtcc-h0g" />
-                    <p className="legend">Legend 3</p>
-                </div>
-                <div>
-                    <img src="https://ideogram.ai/assets/image/lossless/response/tWYEXqIaQ5qnx0dUGEzr3w" />
-                    <p className="legend">Legend 4</p>
-                </div>
-                <div>
-                    <img src="https://ideogram.ai/assets/progressive-image/balanced/response/9k26TJTvTgmaRSGa7QUa8w" />
-                    <p className="legend">Legend 5</p>
-                </div>
-                <div>
-                    <img src="https://ideogram.ai/assets/progressive-image/balanced/response/HlcEVc3nRRukihkLClYDaA" />
-                    <p className="legend">Legend 6</p>
-                </div>
-                <div>
-                    <img src="https://ideogram.ai/assets/image/lossless/response/NEqBp3ZYR1q9Tpr9aBlsiQ" />
-                    <p className="legend">Legend 7</p>
-                </div>
-                <div>
-                    <img src="https://ideogram.ai/assets/progressive-image/balanced/response/jpnef988RoaYikp00AKl-A" />
-                    <p className="legend">Legend 8</p>
-                </div>
-                <div>
-                    <img src="https://ideogram.ai/assets/progressive-image/balanced/response/HDmwQ8XpQTC8TIIcTKMl5g" />
-                    <p className="legend">Legend 9</p>
-                </div>
-                <div>
-                    <img src="https://ideogram.ai/assets/progressive-image/balanced/response/QvFOiLZsSWm56R8-fNZ86A" />
-                    <p className="legend">Legend 10</p>
-                </div>
-                <div>
-                    <img src="https://ideogram.ai/assets/progressive-image/balanced/response/UZu-H8LFRNSeDh8pmr5c6w" />
-                    <p className="legend">Legend 11</p>
-                </div>
-                <div>
-                    <img src="https://ideogram.ai/assets/image/lossless/response/VZP90MJZSYa5MSJJemf29Q" />
-                    <p className="legend">Legend 12</p>
-                </div>
-                <div>
-                    <img src="https://ideogram.ai/assets/image/lossless/response/4jXtql7URvCFBOND7ker2A" />
-                    <p className="legend">Legend 13</p>
-                </div>
-                <div>
-                    <img src="https://ideogram.ai/assets/image/lossless/response/o5yPj8nrSPyuaWZg_EIWwQ" />
-                    <p className="legend">Legend 14</p>
-                </div>
-                <div>
-                    <img src="https://ideogram.ai/assets/image/lossless/response/rFGJmFHPSRqJTvCyBo9kxw" />
-                    <p className="legend">Legend 15</p>
-                </div>
-                <div>
-                    <img src="https://ideogram.ai/assets/image/lossless/response/xJWPn4DPRge5KQryg3O_1w" />
-                    <p className="legend">Legend 16</p>
-                </div>
-            </Carousel>
+            {/* Carousel */}
+            <div className="relative">
+                <Carousel
+                    autoPlay={true}
+                    interval={2000}
+                    infiniteLoop={true}
+                >
+                    <div>
+                        <img src="https://ideogram.ai/assets/image/lossless/response/-sh1EEQ1RyyDohJH_qj42w" />
+                        <p className="legend">Legend 1</p>
+                    </div>
+                    <div>
+                        <img src="https://ideogram.ai/assets/progressive-image/balanced/response/ZQ5XnjQWQPWY-WKQ3t7xKA" />
+                        <p className="legend">Legend 2</p>
+                    </div>
+                    <div>
+                        <img src="https://ideogram.ai/assets/progressive-image/balanced/response/aK6OIb67QmCHZYZtcc-h0g" />
+                        <p className="legend">Legend 3</p>
+                    </div>
+                    <div>
+                        <img src="https://ideogram.ai/assets/image/lossless/response/tWYEXqIaQ5qnx0dUGEzr3w" />
+                        <p className="legend">Legend 4</p>
+                    </div>
+                    <div>
+                        <img src="https://ideogram.ai/assets/progressive-image/balanced/response/9k26TJTvTgmaRSGa7QUa8w" />
+                        <p className="legend">Legend 5</p>
+                    </div>
+                    <div>
+                        <img src="https://ideogram.ai/assets/progressive-image/balanced/response/HlcEVc3nRRukihkLClYDaA" />
+                        <p className="legend">Legend 6</p>
+                    </div>
+                    <div>
+                        <img src="https://ideogram.ai/assets/image/lossless/response/NEqBp3ZYR1q9Tpr9aBlsiQ" />
+                        <p className="legend">Legend 7</p>
+                    </div>
+                    <div>
+                        <img src="https://ideogram.ai/assets/progressive-image/balanced/response/jpnef988RoaYikp00AKl-A" />
+                        <p className="legend">Legend 8</p>
+                    </div>
+                    <div>
+                        <img src="https://ideogram.ai/assets/progressive-image/balanced/response/HDmwQ8XpQTC8TIIcTKMl5g" />
+                        <p className="legend">Legend 9</p>
+                    </div>
+                    <div>
+                        <img src="https://ideogram.ai/assets/progressive-image/balanced/response/QvFOiLZsSWm56R8-fNZ86A" />
+                        <p className="legend">Legend 10</p>
+                    </div>
+                    <div>
+                        <img src="https://ideogram.ai/assets/progressive-image/balanced/response/UZu-H8LFRNSeDh8pmr5c6w" />
+                        <p className="legend">Legend 11</p>
+                    </div>
+                    <div>
+                        <img src="https://ideogram.ai/assets/image/lossless/response/VZP90MJZSYa5MSJJemf29Q" />
+                        <p className="legend">Legend 12</p>
+                    </div>
+                    <div>
+                        <img src="https://ideogram.ai/assets/image/lossless/response/4jXtql7URvCFBOND7ker2A" />
+                        <p className="legend">Legend 13</p>
+                    </div>
+                    <div>
+                        <img src="https://ideogram.ai/assets/image/lossless/response/o5yPj8nrSPyuaWZg_EIWwQ" />
+                        <p className="legend">Legend 14</p>
+                    </div>
+                    <div>
+                        <img src="https://ideogram.ai/assets/image/lossless/response/rFGJmFHPSRqJTvCyBo9kxw" />
+                        <p className="legend">Legend 15</p>
+                    </div>
+                    <div>
+                        <img src="https://ideogram.ai/assets/image/lossless/response/xJWPn4DPRge5KQryg3O_1w" />
+                        <p className="legend">Legend 16</p>
+                    </div>
+                </Carousel>
             </div>
         </div>
     );
--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -1,18 +1,71 @@
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import { EffectFade, Navigation, Pagination } from 'swiper/modules';
 
 const Featured = () => {
     return (
-        <div className="">
-            <div className="">
-                <h1 className="font-bold font-matemasie">Spotlight Creations, Highlighted Masterpieces, Showcasing Exceptional Artworks</h1>
-                <p className="poppins">Explore our curated collection of standout AI-generated artworks. Discover unique pieces, handpicked for their creativity and impact.</p>
+        <div className="mx-auto my-12 drop-shadow-2xl w-[1224px] border-2 border-r-emerald-400 ">
+            <div className="my-12 w-full flex flex-col items-center justify-center text-center ">
+                <h1 className="my-6 text-3xl font-bold font-playfair">Masterpieces, Exceptional Artworks</h1>
+                <p className="mb-12 text-ash-gray font-newAmsterdam">
+                    Discover our featured collection of outstanding artworks, each piece handpicked for its  uniqueness and artistry. <br />These highlighted masterpieces represent the pinnacle of creativity <br /> and innovation from talented artists around the globe.
+                </p>
             </div>
             <div>
                 {/* featured section */}
+                {/* <h1>Illustration</h1> */}
                 <div>
-                <h1 className=" font-raleway">Welcome to Artymia</h1>
-                <p className="font-lato">Explore our curated collection of standout AI-generated artworks.</p>
-
+                    <div className="bg-forest-sunrise w-full flex flex-col items-center justify-center text-center ">
+                        <h1 className="my-6 text-3xl font-bold font-newAmsterdam">Masterpieces, Exceptional Artworks</h1>
+                        <p className="mb-12 font-playfair">
+                            Discover our featured collection of outstanding artworks, each piece handpicked for its  uniqueness and artistry. <br />These highlighted masterpieces represent the pinnacle of creativity <br /> and innovation from talented artists around the globe.
+                        </p>
+                    </div>
+                    <div>
+                    <Swiper
+                        spaceBetween={30}
+                        effect={'fade'}
+                        navigation={true}
+                        pagination={{
+                        clickable: true,
+                        }}
+                        modules={[EffectFade, Navigation, Pagination]}
+                        className="mySwiper"
+                    >
+                        <SwiperSlide>
+                            <div className='relative group'>
+                                <div className="absolute inset-0 flex flex-col p-12 justify-center items-center z-50 transition-opacity duration-300 opacity-0 group-hover:opacity-100">
+                                    <div className='bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% opacity-30 w-full h-full'>
+                                        {/* <h1 className="p-5 bg-magenta-breeze text-3xl font-bungeeTint">Welcome to Artymia</h1>
+                                        <p className="p-5 bg-forest-sunrise mb-8 font-montserrat">Explore our curated collection of standout AI-generated artworks. Discover unique pieces, handpicked for their creativity and impact.</p> */}
+                                    </div>
+                                </div>
+                                <div>
+                                    <img src="https://ideogram.ai/assets/image/lossless/response/eq_Iig4uS-Gw_Fq_tIXpBA" />
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src="https://ideogram.ai/assets/image/lossless/response/82zEfkJDTCG8yJzMa8ICnA" />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src="https://ideogram.ai/assets/image/lossless/response/ex9dMrDHQ9ua4pWy-brPnQ" />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src="https://ideogram.ai/assets/image/lossless/response/t6LEDHTmQMO0gdu-TA0OfA" />
+                        </SwiperSlide>
+                    </Swiper>
+                    </div>
                 </div>
             </div>
         </div>
