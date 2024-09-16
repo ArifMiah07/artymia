e0f9d1862c928cbc6c3d7f05ffed6514affcd52d
ArifMiah07 <arifmiah.me322@gmail.com>
2024-09-17 02:48:56 +0600

    solved infinit wh bug

--- src/Pages/Home/Featured/Features/ArtCards/ArtCard.jsx
+++ src/Pages/Home/Featured/Features/ArtCards/ArtCard.jsx
@@ -1,186 +1,81 @@
-
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-cards';
-
-// import './styles.css';
-
-// import required modules
 import { EffectCards } from 'swiper/modules';
 
-
-import img1 from '../../../../../assets/images/card-img/1.jpg'
-import img2 from '../../../../../assets/images/card-img/2.jpg'
-import img3 from '../../../../../assets/images/card-img/3.jpg'
-import img4 from '../../../../../assets/images/card-img/4.jpg'
-import img5 from '../../../../../assets/images/card-img/5.jpg'
-import img6 from '../../../../../assets/images/card-img/6.jpg'
-import img7 from '../../../../../assets/images/card-img/7.jpg'
-import img8 from '../../../../../assets/images/card-img/8.jpg'
-import img9 from '../../../../../assets/images/card-img/9.jpg'
-import img10 from '../../../../../assets/images/card-img/10.jpg'
-import img11 from '../../../../../assets/images/card-img/11.jpg'
-import img12 from '../../../../../assets/images/card-img/12.jpg'
+import img1 from '../../../../../assets/images/card-img/1.jpg';
+import img2 from '../../../../../assets/images/card-img/2.jpg';
+import img3 from '../../../../../assets/images/card-img/3.jpg';
+// ... other image imports
 import HeaderCard from '../../../../../Components/Cards/HeaderCard/HeaderCard';
+import MiniHeaderCard from '../../../../../Components/Cards/MiniHeaderCard/MiniHeaderCard';
 
 const ArtCard = () => {
     return (
         <div className='mb-24'>
             {/* header section */}
             <header>
-            <HeaderCard headingText={"Edited Art Collection"}></HeaderCard> 
+                <HeaderCard headingText={"Edited Art Collection"}></HeaderCard>
             </header>
             {/* main section */}
             <main className='px-10'>
                 {/* section 1 */}
                 <section className='border border-gray-700'>
-                    <div className='p-24 border-y-4 border-gray-200'>
+                <MiniHeaderCard headerText={"Anime"}></MiniHeaderCard>
+                    <div className='p-8 border-y-4 border-gray-200'>
                         <Swiper
                             effect={'cards'}
                             grabCursor={true}
                             modules={[EffectCards]}
-                            className="mySwiper p-0"
+                            className="mySwiper"
+                            // Setting explicit width and height
+                            style={{
+                                width: '400px',  // You can customize this value
+                                height: '500px', // Custom height
+                            }}
                         >
                             <SwiperSlide>
-                                <div>
-                                    <img src={img1} alt="" />
-                                </div>
+                                <img src={img1} alt="" className="w-full h-full object-cover" />
                             </SwiperSlide>
                             <SwiperSlide>
-                                <div>
-                                    <img src={img2} alt="" />
-                                </div>
+                                <img src={img2} alt="" className="w-full h-full object-cover" />
                             </SwiperSlide>
                             <SwiperSlide>
-                                <div>
-                                    <img src={img3} alt="" />
-                                </div>
+                                <img src={img3} alt="" className="w-full h-full object-cover" />
                             </SwiperSlide>
-                            <SwiperSlide>
-                                <div>
-                                    <img src={img4} alt="" />
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div>
-                                    <img src={img5} alt="" />
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div>
-                                    <img src={img6} alt="" />
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div>
-                                    <img src={img7} alt="" />
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div>
-                                    <img src={img8} alt="" />
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div>
-                                    <img src={img9} alt="" />
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div>
-                                    <img src={img10} alt="" />
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div>
-                                    <img src={img11} alt="" />
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div>
-                                    <img src={img12} alt="" />
-                                </div>
-                            </SwiperSlide>
+                            {/* Add more SwiperSlide components as needed */}
                         </Swiper>
                     </div>
                 </section>
+
                 {/* section 2 */}
                 <section className='border border-gray-700 my-12'>
-                    <div className='p-24 border-y-4 border-gray-200'>
+                <MiniHeaderCard headerText={"Anime"}></MiniHeaderCard>
+                    <div className='p-8 border-y-4 border-gray-200'>
                         <Swiper
                             effect={'cards'}
                             grabCursor={true}
                             modules={[EffectCards]}
-                            className="mySwiper p-0"
+                            className="mySwiper"
+                            // Setting explicit width and height
+                            style={{
+                                width: '400px',
+                                height: '500px',
+                            }}
                         >
                             <SwiperSlide>
-                                <div>
-                                    <img src={img1} alt="" />
-                                </div>
+                                <img src={img1} alt="" className="w-full h-full object-cover" />
                             </SwiperSlide>
                             <SwiperSlide>
-                                <div>
-                                    <img src={img2} alt="" />
-                                </div>
+                                <img src={img2} alt="" className="w-full h-full object-cover" />
                             </SwiperSlide>
                             <SwiperSlide>
-                                <div>
-                                    <img src={img3} alt="" />
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div>
-                                    <img src={img4} alt="" />
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div>
-                                    <img src={img5} alt="" />
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div>
-                                    <img src={img6} alt="" />
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div>
-                                    <img src={img7} alt="" />
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div>
-                                    <img src={img8} alt="" />
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div>
-                                    <img src={img9} alt="" />
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div>
-                                    <img src={img10} alt="" />
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div>
-                                    <img src={img11} alt="" />
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div>
-                                    <img src={img12} alt="" />
-                                </div>
+                                <img src={img3} alt="" className="w-full h-full object-cover" />
                             </SwiperSlide>
                         </Swiper>
                     </div>
                 </section>
-            </main>       
-            
+            </main>
         </div>
     );
 };
